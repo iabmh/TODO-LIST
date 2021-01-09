10c66c18ae13d49a4eb7072d7d07840b1ed9eac0
Abbas Mh <75030396+iabmh@users.noreply.github.com>
2021-01-09 19:23:32 +0530

    Add files via upload

---
+++ todo/tasks/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+
+class taskForm(forms.ModelForm):
+    
+    class Meta:
+        model = task
+        fields = ("__all__")
+
